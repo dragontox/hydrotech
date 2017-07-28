1dcfac16003a74dad74ef870eaee86da38235bab
Unknown <tristanhollman@gmail.com>
2017-07-29 01:05:43 +0200

    don't break glass

--- src/main/java/tresy/deathnote/common/mods/BlockBreakMessage.java
+++ src/main/java/tresy/deathnote/common/mods/BlockBreakMessage.java
@@ -1,5 +1,7 @@
 package tresy.deathnote.common.mods;
 
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandKill;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.event.world.BlockEvent.BreakEvent;
@@ -12,9 +14,25 @@ public class BlockBreakMessage {
     
     @SubscribeEvent
     public void sendMessage(BreakEvent breakEvent) {
+        
+        if (breakEvent.getState().getBlock().getUnlocalizedName().equalsIgnoreCase("tile.glass")) {
+            brokeGlass(breakEvent);
+        } else {
+            brokeOther(breakEvent);
+        }
+    }
+    
+    public void brokeGlass(BreakEvent breakEvent) {
+        
+        breakEvent.getPlayer().sendMessage(new TextComponentString(TextFormatting.RED + "YOU BROKE GLASS, DIE PIECE OF SHIT!"));
+        breakEvent.getPlayer().onKillCommand();
+    }
+    
+    public void brokeOther(BreakEvent breakEvent) {
         String message = "You broke a piece of the world: ";
-        String blockName = breakEvent.getState().getBlock().getLocalizedName();
+        String blockName = breakEvent.getState().getBlock().getLocalizedName() + " " + breakEvent.getState().getBlock().getUnlocalizedName();
         message += blockName;
+        
         breakEvent.getPlayer().sendMessage(new TextComponentString(TextFormatting.AQUA + message));
     }
 }
