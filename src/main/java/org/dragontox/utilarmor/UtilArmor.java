667e496b2df6777c49bcbbf4409896a75a1e7922
drag <thedragontox1997@gmail.com>
2017-08-04 04:02:05 +0200

    Added creative tab to main file

--- src/main/java/org/dragontox/utilarmor/UtilArmor.java
+++ src/main/java/org/dragontox/utilarmor/UtilArmor.java
@@ -1,11 +1,15 @@
 package org.dragontox.utilarmor;
 
 
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.dragontox.utilarmor.creativeTab.creativeTabUtilArmor;
 import org.dragontox.utilarmor.proxy.CommonProxy;
 
 @Mod(modid = UtilArmor.MODID, version = UtilArmor.VERSION)
@@ -13,14 +17,25 @@ public class UtilArmor
 {
     public static final String MODID = "utilarmor";
     public static final String VERSION = "0.01";
+    public static creativeTabUtilArmor TabUtilArmor;
 
     @SidedProxy(clientSide = "org.dragontox.utilarmor.proxy.ClientProxy" , serverSide = "org.dragontox.ultilarmor.proxy.CommonProxy")
     public static CommonProxy proxy;
+
+    @EventHandler
+    public void preinit(FMLPreInitializationEvent event){
+        TabUtilArmor = new creativeTabUtilArmor(CreativeTabs.getNextID(), "Util_Armor");
+    }
 
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
         // some example code
         System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
+    }
+    @EventHandler
+    public void postinit(FMLPostInitializationEvent event)
+    {
+
     }
 }
--- src/main/java/org/dragontox/utilarmor/creativeTab/creativeTabUtilArmor.java
+++ src/main/java/org/dragontox/utilarmor/creativeTab/creativeTabUtilArmor.java
@@ -1,4 +1,17 @@
 package org.dragontox.utilarmor.creativeTab;
 
-public class creativeTabUtilArmor {
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+
+public class creativeTabUtilArmor extends CreativeTabs {
+    public creativeTabUtilArmor(int index, String label) {
+        super(index, label);
+    }
+
+    @Override
+    public ItemStack getTabIconItem() {
+
+        return new ItemStack(Items.DIAMOND_CHESTPLATE);
+    }
 }
